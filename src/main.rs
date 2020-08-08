4a9ab803fbf842fec2df85cc9b3dee27338ca479
Cameron Taggart <cameron.taggart@gmail.com>
2020-08-08 10:55:39 -0600

    add jsonrpc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use jsonrpc_core::{IoHandler, Params, Value};
 use std::{io, io::prelude::*};
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub fn main() -> Result<()> {
-    for line in io::stdin().lock().lines() {
-        println!("length = {}", line?.len());
+    let mut handler = IoHandler::new();
+    handler.add_sync_method("say_hello", |_: Params| Ok(Value::String("Hello World!".to_owned())));
+
+    for req in io::stdin().lock().lines() {
+        if let Some(rsp) = handler.handle_request_sync(&req?) {
+            println!("{}", rsp);
+        }
     }
     Ok(())
 }

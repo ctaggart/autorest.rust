103e10f2a91e9146d588269356e8e3c4f2eabe85
Cameron Taggart <cameron.taggart@gmail.com>
2020-08-08 10:28:04 -0600

    use std::io

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use tokio::io::{self, AsyncBufReadExt, BufReader};
+use std::{io, io::prelude::*};
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[tokio::main]
-pub async fn main() -> Result<()> {
-    let stdin = io::stdin();
-    let mut lines = BufReader::new(stdin).lines();
-
-    while let Some(line) = lines.next_line().await? {
-        println!("length = {}", line.len());
+pub fn main() -> Result<()> {
+    for line in io::stdin().lock().lines() {
+        println!("length = {}", line?.len());
     }
-
     Ok(())
 }

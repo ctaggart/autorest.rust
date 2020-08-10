6da0cf80bb0ea80798c90bbaf624c5de0655dc07
Cameron Taggart <cameron.taggart@gmail.com>
2020-08-10 15:22:37 -0600

    rename to autorust

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const { WASI } = require("wasi");
 const wasi = new WASI({
   args: process.argv,
   env: process.env,
+  preopens: {
+    "/sandbox": process.cwd()
+  }
 });
 const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
 (async () => {
   const wasm = await WebAssembly.compile(
-    fs.readFileSync("target/wasm32-wasi/debug/autorest-rust.wasm")
+    fs.readFileSync("target/wasm32-wasi/debug/autorust.wasm")
   );
   const instance = await WebAssembly.instantiate(wasm, importObject);
   wasi.start(instance);
